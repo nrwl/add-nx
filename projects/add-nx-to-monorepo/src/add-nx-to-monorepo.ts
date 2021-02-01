fd73a5fc1171ec2128078cdf6d63fc5b26b1a505
Victor Savkin <vic.savkin@gmail.com>
2021-02-01 13:28:08 -0500

    update the learn more link

--- projects/add-nx-to-monorepo/src/add-nx-to-monorepo.ts
+++ projects/add-nx-to-monorepo/src/add-nx-to-monorepo.ts
@@ -334,7 +334,7 @@ function printFinalMessage(repoRoot) {
       } nx run-many --target=build --all --parallel" to run the build script for every project in the monorepo.`,
       `- Run it again to replay the cached computation.`,
       `- Run "nx dep-graph" to see the structure of the monorepo.`,
-      `- Learn more at https://nx.dev/react`,
+      `- Learn more at https://nx.dev/migration/adding-to-monorepo`,
     ],
   });
 }
